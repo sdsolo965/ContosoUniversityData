31a7e7eb10ace85e90bbba1419a138371f4e36d7
Shane Solomon <shanedouglas626@gmail.com>
2018-04-30 10:02:22 -0400

    InitialCommit

---
+++ ContosoSite/Models/PartialClasses.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ContosoSite.Models
+{
+    public class PartialClasses
+    {
+        [MetadataType(typeof(Metadata.StudentMetadata))]
+        public partial class Student
+        {
+        }
+
+        [MetadataType(typeof(Metadata.EnrollmentMetadata))]
+        public partial class Enrollment
+        {
+        }
+    }
+}
